6a2ad5698ec11f61b6795306689d65f553c92b04
Farwent <philipp_ehm@protonmail.com>
2019-09-13 00:21:06 +0200

    Week2: Decision Boundary plot / Optimize function from scipy

--- exercises/week2/cost_function_logistic_regression.py
+++ exercises/week2/cost_function_logistic_regression.py
@@ -1,5 +1,8 @@
 import numpy as np
 import os
+from matplotlib import pyplot
+# Optimization module in scipy
+from scipy import optimize
 
 
 def sigmoid(z):
@@ -76,6 +79,64 @@ def costFunction(theta, X, y):
     return J, grad
 
 
+def plotDecisionBoundary(theta, X, y):
+    """
+    Plots the data points X and y into a new figure with the decision boundary defined by theta.
+    Plots the data points with * for the positive examples and o for  the negative examples.
+
+    Parameters
+    ----------
+    plotData : func
+        A function reference for plotting the X, y data.
+
+    theta : array_like
+        Parameters for logistic regression. A vector of shape (n+1, ).
+
+    X : array_like
+        The input dataset. X is assumed to be  a either:
+            1) Mx3 matrix, where the first column is an all ones column for the intercept.
+            2) MxN, N>3 matrix, where the first column is all ones.
+
+    y : array_like
+        Vector of data labels of shape (m, ).
+    """
+    # make sure theta is a numpy array
+    theta = np.array(theta)
+
+    # Plot Data (remember first column in X is the intercept)
+    plotData(X[:, 1:3], y)
+
+    if X.shape[1] <= 3:
+        # Only need 2 points to define a line, so choose two endpoints
+        plot_x = np.array([np.min(X[:, 1]) - 2, np.max(X[:, 1]) + 2])
+        print(plot_x)
+
+        # Calculate the decision boundary line
+        plot_y = (-1. / theta[2]) * (theta[1] * plot_x + theta[0])
+        print(plot_y)
+
+        # Plot, and adjust axes for better viewing
+        pyplot.plot(plot_x, plot_y)
+
+        # Legend, specific for the exercise
+        pyplot.legend(['Admitted', 'Not admitted', 'Decision Boundary'])
+        pyplot.xlim([30, 100])
+        pyplot.ylim([30, 100])
+
+
+def plotData(X, y):
+    # Create New Figure
+    fig = pyplot.figure()
+
+    pos = y == 1
+    neg = y == 0
+
+    pyplot.plot(X[pos, 0], X[pos, 1], 'og')
+    pyplot.plot(X[neg, 0], X[neg, 1], 'ro')
+    pyplot.xlabel("First Exam")
+    pyplot.ylabel("Second Exam")
+
+
 if __name__ == '__main__':
     data = np.loadtxt(os.path.join("/home/thelichking/Desktop/ml-coursera-python-assignments/Exercise2/Data",
                                    "ex2data1.txt"), delimiter=',')
@@ -109,3 +170,37 @@ if __name__ == '__main__':
     print('Gradient at test theta:')
     print('\t[{:.3f}, {:.3f}, {:.3f}]'.format(*grad))
     print('Expected gradients (approx):\n\t[0.043, 2.566, 2.647]')
+
+    # set options for optimize.minimize
+    options = {'maxiter': 400}
+
+    # see documention for scipy's optimize.minimize  for description about
+    # the different parameters
+    # The function returns an object `OptimizeResult`
+    # We use truncated Newton algorithm for optimization which is
+    # equivalent to MATLAB's fminunc
+    # See https://stackoverflow.com/questions/18801002/fminunc-alternate-in-numpy
+    res = optimize.minimize(costFunction,
+                            initial_theta,
+                            (X, y),
+                            jac=True,
+                            method='TNC',
+                            options=options)
+
+    # the fun property of `OptimizeResult` object returns
+    # the value of costFunction at optimized theta
+    cost = res.fun
+
+    # the optimized theta is in the x property
+    theta = res.x
+
+    # Print theta to screen
+    print('Cost at theta found by optimize.minimize: {:.3f}'.format(cost))
+    print('Expected cost (approx): 0.203\n');
+
+    print('theta:')
+    print('\t[{:.3f}, {:.3f}, {:.3f}]'.format(*theta))
+    print('Expected theta (approx):\n\t[-25.161, 0.206, 0.201]')
+
+    plotDecisionBoundary(theta,X,y)
+    pyplot.show()
